3891bffc454a448c3ec04ac06796c81b22109ef6
Wei Feng <beyondfengwei@gmail.com>
2016-03-12 17:25:54 +0800

    firt commit

---
+++ WEB-INF/src/vocabtrainer/interactionmodule/WelcomeServlet.java
@@ -0,0 +1,79 @@
+package vocabtrainer.interactionmodule;
+
+import vocabtrainer.rlmodule.RunVocabTrainerNoSockets;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+
+    /**
+     * The doGet method of the servlet. <br>
+     *
+     * This method is called when a form has its tag value method equals to get.
+     * 
+     * @param request the request send by the client to the server
+     * @param response the response send by the server to the client
+     * @throws ServletException if an error occurred
+     * @throws IOException if an error occurred
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		
+		RunVocabTrainerNoSockets run = new RunVocabTrainerNoSockets();
+        // 设置响应内容类型
+		response.setContentType("text/html;charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+		String title = "use GET method to get the value";
+		String docType =
+		"<!doctype html public \"-//w3c//dtd html 4.0 " +
+		"transitional//en\">\n";
+		out.println(docType +
+		    "<html>\n" +
+		    "<head><title>" + title + "</title></head>\n" +
+		    "<body bgcolor=\"#f0f0f0\">\n" +
+		    "<h1 align=\"center\">" + title + "</h1>\n" +
+		    "<ul>\n" +
+		    "  <li><b>the value is</b>："
+		    + request.getParameter("value") + "\n" +
+		    "</ul>\n" +
+		    "</body></html>");
+
+		out.println(run.test());
+		
+    }
+
+    /**
+     * The doPost method of the servlet. <br>
+     *
+     * This method is called when a form has its tag value method equals to post.
+     * 
+     * @param request the request send by the client to the server
+     * @param response the response send by the server to the client
+     * @throws ServletException if an error occurred
+     * @throws IOException if an error occurred
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the POST method");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
+
+}
---
+++ WEB-INF/src/vocabtrainer/interactionmodule/WordListGenerator.java
@@ -0,0 +1,65 @@
+package vocabtrainer.interactionmodule;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class WordListGenerator {
+    private List<String> wordList = new ArrayList<String>();
+
+    public String getRandomWord() {
+        Random randGen = new Random();
+        return wordList.get(randGen.nextInt(306));
+
+    }
+
+    public void readFile()
+    {
+        // The name of the file to open.
+        try {
+            ClassLoader.getSystemClassLoader().getResource("wordlist.txt");
+
+        }
+        catch(IOError ex){
+            System.out.println("Resource cannot be loaded");
+        }
+        URL fileName =  ClassLoader.getSystemClassLoader().getResource("wordlist.txt");
+        ;
+        
+        int wordListID = 0;
+        // This will reference one line at a time
+        String line = null;
+
+
+        try {
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(fileName.openStream()));
+
+            while((line = bufferedReader.readLine()) != null) {
+                wordList.add(line);
+                wordListID++;
+                //System.out.println(line);
+
+            }
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+    }
+ }
+    
